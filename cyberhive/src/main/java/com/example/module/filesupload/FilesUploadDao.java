88bb01aafda1f30c73ffba2d80112ab3fbc61c53
ShrimpAde <jiho6202@gmail.com>
2025-05-28 14:29:09 +0900

    source

---
+++ cyberhive/src/main/java/com/example/module/filesupload/FilesUploadDao.java
@@ -0,0 +1,10 @@
+package com.example.module.filesupload;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FilesUploadDao {
+
+//	public int insert(FilesUploadDto dto);
+	public int insertUploaded(FilesUploadDto dto);
+}
